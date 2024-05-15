5fa3e568c95d42481613037e4c6c5bd7694ed938
USER <chuanzhe03@gmail.com>
2024-05-15 18:15:15 +0800

    lab 5

--- android/app/src/androidTest/java/com.example.lab_5_2/MainActivityTest.java
+++ android/app/src/androidTest/java/com.example.lab_5_2/MainActivityTest.java
@@ -1,13 +1,14 @@
 package com.example.lab_5_2;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
+// Import MainActivity from YOUR package
+import com.resocoder.integration_testing_prep.MainActivity;
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
     @Rule
-    public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
 }
