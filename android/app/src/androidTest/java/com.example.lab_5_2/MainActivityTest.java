5d7b4f713fd42f051aa943376cca951f6ef19877
USER <chuanzhe03@gmail.com>
2024-05-15 18:00:11 +0800

    lab 5

--- android/app/src/androidTest/java/com.example.lab_5_2/MainActivityTest.java
+++ android/app/src/androidTest/java/com.example.lab_5_2/MainActivityTest.java
@@ -2,7 +2,7 @@ package com.example.lab_5_2;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import dev.flutter.plugins.integration_test.FlutterTestRunner
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
