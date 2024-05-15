6b2605096f603eb36847e0084f1d59cf4244663a
USER <chuanzhe03@gmail.com>
2024-05-15 18:40:04 +0800

    lab 5

--- android/app/src/androidTest/java/com.example.lab_5_2/MainActivityTest.java
+++ android/app/src/androidTest/java/com.example.lab_5_2/MainActivityTest.java
@@ -4,8 +4,6 @@ import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
-// Import MainActivity from YOUR package
-import com.resocoder.integration_testing_prep.MainActivity;
 
 @RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
