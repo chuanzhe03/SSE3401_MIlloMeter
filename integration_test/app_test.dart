226d4fc2f492a450d67c6d1c1a4aca129619a3f0
USER <chuanzhe03@gmail.com>
2024-05-16 21:29:03 +0800

    lab 5

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -17,26 +17,24 @@ void main() {
         app.main();
         await tester.pumpAndSettle();
         await tester.enterText(find.byType(IntlPhoneField), "7583792224");
-        await Future.delayed(const Duration(seconds: 3));
         await tester.tap(find.byType(Checkbox));
-        await Future.delayed(const Duration(seconds: 2));
         await tester.tap(find.byType(ElevatedButton));
         await tester.pumpAndSettle();
 
         expect(find.byType(activePage), findsOneWidget);
 
         await tester.enterText(find.byType(TextField).at(0), "2");
-        await Future.delayed(const Duration(seconds: 1));
+
         await tester.enterText(find.byType(TextField).at(1), "1");
-        await Future.delayed(const Duration(seconds: 1));
+
         await tester.enterText(find.byType(TextField).at(2), "4");
-        await Future.delayed(const Duration(seconds: 1));
+
         await tester.enterText(find.byType(TextField).at(3), "2");
-        await Future.delayed(const Duration(seconds: 1));
+
         await tester.enterText(find.byType(TextField).at(4), "2");
-        await Future.delayed(const Duration(seconds: 1));
+
         await tester.enterText(find.byType(TextField).at(5), "1");
-        await Future.delayed(const Duration(seconds: 2));
+
         await tester.tap(find.byType(ElevatedButton));
         await tester.pumpAndSettle();
 
